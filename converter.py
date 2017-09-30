125a4c073e462421737c94670300e231b73275f9
Ryan Monaghan <monaghanryan95@gmail.com>
2017-09-30 22:39:41 +0100

    Initial commit

---
+++ converter.py
@@ -0,0 +1,13 @@
+#Author:
+
+#Purpose: To convert Celsius into Fahrenheit.
+
+#Algorithm: Fahrenheit = Celsius *9/6+32
+
+#License:
+#---------------------------------------------
+print "Celsius to Fahrenheit Converter"
+print "-------------------------------"
+celsius = float(raw_input("Enter the temperature in Celsius:"))
+fahrenheit = ((celsius*9)/5)+32
+print "Fahrenheit:",fahrenheit
